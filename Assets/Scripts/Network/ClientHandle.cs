57bc05f719a9ba7fe810e4c070d87e17ad0b7846
PochitaOP <59409127+PochitaOP@users.noreply.github.com>
2020-11-02 10:58:13 +0000

    ClientSave01

---
+++ Assets/Scripts/Network/ClientHandle.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+
+public class ClientHandle : MonoBehaviour
+{
+    //ClientHadle class will process the packet that has been received to be read and use the data sent from the server.
+    public static void Welcome(Packet _packet)
+    {
+        //note: write the code in order the same way from ServerSend class at least for TCP packets
+
+        string _msg = _packet.ReadString();
+        int _myId = _packet.ReadInt();
+
+        Debug.Log($"Message from the server: {_msg}");
+        Client.instance.myId = _myId;
+        ClientSend.WelcomeReceived();
+
+        Client.instance.udp.Connect(((IPEndPoint)Client.instance.tcp.socket.Client.LocalEndPoint).Port);
+    }
+
+    public static void SpawnPlayer(Packet _packet)
+    {
+        int _id = _packet.ReadInt();
+        string _username = _packet.ReadString();
+        Vector2 _position = _packet.ReadVector2();
+
+        GameManager.instance.SpawnPlayer(_id, _username, _position);
+    }
+
+    public static void PlayerPosition(Packet _packet)
+    {
+        int _id = _packet.ReadInt();
+        Vector3 _position = _packet.ReadVector3();
+        GameManager.players[_id].transform.position = _position;
+    }
+
+    public static void PlayerRotation(Packet _packet)
+    {
+        int _id = _packet.ReadInt();
+        Quaternion _rotation = _packet.ReadQuaternion();
+        GameManager.players[_id].transform.rotation = _rotation;
+    }
+
+    public static void removePlayerObject(Packet _packet)
+    {
+        int _id = _packet.ReadInt();
+        Destroy(GameManager.players[_id].gameObject);
+        GameManager.players.Remove(_id);
+    }
+}
